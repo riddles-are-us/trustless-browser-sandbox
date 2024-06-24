1f70bae54b548f459e64ded60bdac3389e613563
akayi07 <zhuolimiaotx@163.com>
2024-06-24 10:46:41 +0800

    replace BigInt with bn.js for precision

--- src/games/miniserverfe/helper.tsx
+++ src/games/miniserverfe/helper.tsx
@@ -1,17 +1,22 @@
+import BN from "bn.js";
+
 export function getModifierIndex(modifierInfo: number) {
-  const currentMInfoBinary = BigInt(modifierInfo).toString(2).padStart(64, "0");
+  const modifierInfoBN = new BN(modifierInfo.toString());
+  const currentMInfoBinary = modifierInfoBN.toString(2, 64);
   const currentMIndex = parseInt(currentMInfoBinary.slice(1, 8), 2);
   return currentMIndex;
 }
 
 export function getHaltBit(modifierInfo: number) {
-  const currentMInfoBinary = BigInt(modifierInfo).toString(2).padStart(64, "0");
+  const modifierInfoBN = new BN(modifierInfo.toString());
+  const currentMInfoBinary = modifierInfoBN.toString(2, 64);
   const haltBit = Number(currentMInfoBinary.charAt(0));
   return haltBit;
 }
 
 export function getCounter(modifierInfo: number) {
-  const currentMInfoBinary = BigInt(modifierInfo).toString(2).padStart(64, "0");
+  const modifierInfoBN = new BN(modifierInfo.toString());
+  const currentMInfoBinary = modifierInfoBN.toString(2, 64);
   const counter = parseInt(currentMInfoBinary.slice(8), 2);
   return counter;
 }
