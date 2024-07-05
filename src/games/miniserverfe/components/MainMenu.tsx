8971850d5ff245cbc959eaa8b4cdd070a3426b5d
charlie275691502 <charlie275691502@gmail.com>
2024-07-06 02:52:42 +1000

    main button

--- src/games/miniserverfe/components/MainMenu.tsx
+++ src/games/miniserverfe/components/MainMenu.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import circle from "../images/MainMenu/circle.png";
+import confirmButtonImage from "../images/MainMenu/confirm.png";
+import confirmButtonHoverImage from "../images/MainMenu/confirm_hover.png";
+import rebootButtonImage from "../images/MainMenu/reboot.png";
+import rebootButtonHoverImage from "../images/MainMenu/reboot_hover.png";
 import "./MainMenu.css";
 
 const MainMenu = () => {
   return (
     <div className="main-background">
       <div className="main-content">
-        <img src={circle} className="circle-background" />
+        <img src={circle} className="main-circle-background" />
+        <img src={confirmButtonImage} className="main-button-image" />
       </div>
     </div>
   );
