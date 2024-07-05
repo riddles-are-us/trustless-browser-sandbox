b0a61b790d6be6365bf51d69a30389bb77b7e40d
charlie275691502 <charlie275691502@gmail.com>
2024-07-06 02:41:41 +1000

    add circle background

--- src/games/miniserverfe/components/MainMenu.tsx
+++ src/games/miniserverfe/components/MainMenu.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import circle from "../images/MainMenu/circle.png";
 import "./MainMenu.css";
 
 const MainMenu = () => {
   return (
     <div className="main-background">
       <div className="main-content">
+        <img src={circle} className="circle-background" />
       </div>
     </div>
   );
