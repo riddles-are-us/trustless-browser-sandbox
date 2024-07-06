1eb37453476bc059713311af759dd2fc5f19c921
charlie275691502 <charlie275691502@gmail.com>
2024-07-06 14:53:56 +1000

    add main display image

--- src/games/miniserverfe/components/MainMenu.tsx
+++ src/games/miniserverfe/components/MainMenu.tsx
@@ -4,6 +4,7 @@ import confirmButtonImage from "../images/MainMenu/confirm.png";
 import confirmButtonHoverImage from "../images/MainMenu/confirm_hover.png";
 import rebootButtonImage from "../images/MainMenu/reboot.png";
 import rebootButtonHoverImage from "../images/MainMenu/reboot_hover.png";
+import display from "../images/MainMenu/display.png";
 import "./MainMenu.css";
 import MainMenuProgressBar from "./MainMenuProgressBar";
 
@@ -12,8 +13,9 @@ const MainMenu = () => {
     <div className="main-background">
       <div className="main-content">
         <img src={circle} className="main-circle-background" />
-        <img src={confirmButtonImage} className="main-button-image" />
+        <img src={display} className="main-display-image" />
         <MainMenuProgressBar />
+        <img src={confirmButtonImage} className="main-button-image" />
       </div>
     </div>
   );
