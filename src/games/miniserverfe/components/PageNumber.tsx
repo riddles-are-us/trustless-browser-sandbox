553a49a3b69b488c5eba378efb316710fc4cd46d
charlie275691502 <charlie275691502@gmail.com>
2024-07-09 17:15:57 +1000

    Add page Count

---
+++ src/games/miniserverfe/components/PageNumber.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import number_display from "../images/backgrounds/number_display.png";
+import "./PageNumber.css";
+
+interface Props {
+  page: number;
+  maxPage: number;
+  positionClass: string;
+}
+
+const PageNumber = ({ page, maxPage, positionClass }: Props) => {
+  return (
+    <>
+      <img
+        src={number_display}
+        className={`page-number-background ${positionClass}`}
+      ></img>
+
+      <p
+        className={`page-number-text ${positionClass}`}
+      >{`${page} / ${maxPage}`}</p>
+    </>
+  );
+};
+
+export default PageNumber;
--- src/games/miniserverfe/components/RightMenu.tsx
+++ src/games/miniserverfe/components/RightMenu.tsx
@@ -3,6 +3,7 @@ import rightCornerBar from "../images/backgrounds/right_corner_bar.png";
 import "./RightMenu.css";
 import UpButton from "./Buttons/UpButton";
 import DownButton from "./Buttons/DownButton";
+import PageNumber from "./PageNumber";
 
 const RightMenu = () => {
   return (
@@ -11,6 +12,11 @@ const RightMenu = () => {
       <div className="right-middle"></div>
       <div className="right-bottom"></div>
       <img src={rightCornerBar} className="right-corner-bar" />
+      <PageNumber
+        page={1}
+        maxPage={8}
+        positionClass={"right-page-number-position"}
+      />
       <UpButton positionClass={"right-up-button-position"} />
       <DownButton positionClass={"right-down-button-position"} />
     </div>
