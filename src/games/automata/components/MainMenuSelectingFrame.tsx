d8545431a32e3b7fa3091daaa5c97128e258f505
charlie275691502 <charlie275691502@gmail.com>
2024-08-08 15:20:40 +1000

    change arrow to selecting frame

--- src/games/automata/components/MainMenuSelectingFrame.tsx
+++ src/games/automata/components/MainMenuSelectingFrame.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import arrow from "../images/MainMenu/arrow.png";
-import selectingFrame from "../images/MainMenu/selecting_frame.png";
-import selectingFrameStop from "../images/MainMenu/selecting_frame_red.png";
+import currentFrame from "../images/MainMenu/selecting_frame.png";
+import currentFrameStop from "../images/MainMenu/selecting_frame_red.png";
+import selectingFrame from "../images/MainMenu/select.png";
 
 import "./MainMenuSelectingFrame.css";
 
@@ -19,47 +19,59 @@ const MainMenuSelectingFrame = ({ order, isCurrentProgram, isStop }: Props) => {
   const rotation = order * 45 + 22.5;
   const angle = 90 - rotation;
 
-  const selectingFrameRadius = 30;
-  const selectingFrameYPosition =
-    50 - Math.sin((angle * Math.PI) / 180) * selectingFrameRadius;
-  const selectingFrameXPosition =
-    50 + Math.cos((angle * Math.PI) / 180) * selectingFrameRadius;
+  const currentFrameRadius = 30;
+  const currentFrameYPosition =
+    50 - Math.sin((angle * Math.PI) / 180) * currentFrameRadius;
+  const currentFrameXPosition =
+    50 + Math.cos((angle * Math.PI) / 180) * currentFrameRadius;
 
-  const arrowRadius = 13.5;
-  const arrowYPosition = 50 - Math.sin((angle * Math.PI) / 180) * arrowRadius;
-  const arrowXPposition = 50 + Math.cos((angle * Math.PI) / 180) * arrowRadius;
-  return (
-    <>
-      {isCurrentProgram && (
-        <div
-          className="main-selecting-frame-container"
-          style={{
-            top: `${selectingFrameYPosition}%`,
-            left: `${selectingFrameXPosition}%`,
-          }}
-        >
-          <img
-            src={isStop ? selectingFrameStop : selectingFrame}
-            className="main-selecting-frame-image"
-            style={{
-              transform: `translate(-50%, -50%) rotate(${rotation}deg)`,
-            }}
-          />
-        </div>
-      )}
+  const selectingRadius = 29;
+  const selectingYPosition =
+    50 - Math.sin((angle * Math.PI) / 180) * selectingRadius;
+  const selectingXPosition =
+    50 + Math.cos((angle * Math.PI) / 180) * selectingRadius;
 
+  const currentFrameDiv = () => {
+    return (
       <div
-        className="main-arrow-container"
-        style={{ top: `${arrowYPosition}%`, left: `${arrowXPposition}%` }}
+        className="main-current-frame-container"
+        style={{
+          top: `${currentFrameYPosition}%`,
+          left: `${currentFrameXPosition}%`,
+        }}
       >
         <img
-          src={arrow}
-          className="main-arrow-image"
-          style={{ transform: `translate(-50%, -50%) rotate(${rotation}deg)` }}
+          src={isStop ? currentFrameStop : currentFrame}
+          className="main-current-frame-image"
+          style={{
+            transform: `translate(-50%, -50%) rotate(${rotation}deg)`,
+          }}
         />
       </div>
-    </>
-  );
+    );
+  };
+
+  const selectingFrameDiv = () => {
+    return (
+      <div
+        className="main-selecting-frame-container"
+        style={{
+          top: `${selectingYPosition}%`,
+          left: `${selectingXPosition}%`,
+        }}
+      >
+        <img
+          src={selectingFrame}
+          className="main-selecting-frame-image"
+          style={{
+            transform: `translate(-50%, -50%) rotate(${rotation}deg)`,
+          }}
+        />
+      </div>
+    );
+  };
+
+  return <>{isCurrentProgram ? currentFrameDiv() : selectingFrameDiv()}</>;
 };
 
 export default MainMenuSelectingFrame;
